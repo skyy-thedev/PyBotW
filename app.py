d60e7efc71ecd0dda0f603c4b78005f0b9999d37
skyy_dev <skyzeduardo@gmail.com>
2024-01-20 15:24:15 -0300

    pyBot who write json & chat whatsapp

---
+++ app.py
@@ -0,0 +1,38 @@
+import json
+from urllib.parse import quote
+import webbrowser
+from time import sleep
+import pyautogui 
+import os 
+
+# Ler dados do arquivo JSON
+with open('clientes.json', 'r', encoding='utf-8') as arquivo_json:
+    dados_clientes = json.load(arquivo_json)
+
+webbrowser.open('https://web.whatsapp.com/')
+sleep(30)
+
+for cliente in dados_clientes:
+    # Extrair informações sobre nome, telefone e data de vencimento
+    nome = cliente['Nome']
+    telefone = cliente['Telefone']
+    vencimento = cliente['DataVencimento']
+    
+    mensagem = f'Olá {nome}, seu boleto vence no dia {vencimento}. Favor pagar no link https://www.link_do_pagamento.com'
+
+    # Criar links personalizados do WhatsApp e enviar mensagens para cada cliente
+    # com base nos dados do arquivo JSON
+    try:
+        link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+        webbrowser.open(link_mensagem_whatsapp)
+        sleep(12)
+        seta = pyautogui.locateCenterOnScreen('seta.bmp')
+        sleep(7)
+        pyautogui.click(seta[0], seta[1])
+        sleep(10)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(10)
+    except:
+        print(f'Não foi possível enviar mensagem para {nome}')
+        with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
+            arquivo.write(f'{nome},{telefone}{os.linesep}')
